21db4b135f729d9c4d3b48e5dc42ab8d51bd2ab8
Ashluna22 <ashleyvera032@gmail.com>
2024-08-14 02:09:55 -0400

    first push

---
+++ Hello.world/Hello.world/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+
+
+
+namespace Hello.world
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+            do
+            {
+                Console.WriteLine("Hello, World! Would you like to continue (y/n)?");
+                userInput = Console.ReadLine();
+
+                if (userInput == "y")
+                {
+                    continue;
+                }
+
+
+            } while (userInput != "n");
+            Console.WriteLine("Bye bye!");
+        }
+
+    }
+}
+
+        
